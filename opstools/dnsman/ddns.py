3d013d018cab4dde7cef116e5a36bc93d879b6df
epic2005 <lipengyue@gmail.com>
2013-08-16 16:38:30 -0700

    dnsman homework not finish

--- opstools/dnsman/ddns.py
+++ opstools/dnsman/ddns.py
@@ -4,4 +4,13 @@ import dns.update
 import dns.tsigkeyring
 
 key = "BksoRXYLfOcUla1fmwlfv0loL+B1ceqvAQpd/7DgEYzCRGQasbUxQZbP +BFlFkAHyvHOZS5LyMxpPywO6UOQKQ=="
+keyring = dns.tsigkeyring.from_text({'keyname' : key})
 
+def dman(zone, name, ttl, type, rr, delete=False):
+    if type == 'cname' and len(rr) == 1:
+        update = dns.update.Update(name, keyring=keyring)
+        update.replace('host', ttl, type, rr)
+
+
+#dman('111','abc',600,'cname',['192.168.1.1'] )
+    
