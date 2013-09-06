4025e14733da338d0b532e6abc49402deda9a0fe
lipengyue <lipengyue@gmail.com>
2013-09-07 00:03:08 +0800

    fab homework

--- opstools/fabric/fabfile.py
+++ opstools/fabric/fabfile.py
@@ -1,33 +1,50 @@
 #!/usr/bin/env python
-
 from fabric.api import *
 import os
 
 env.username = 'root'
 env.password = 'aaasss'
 
-myhosts =  ['172.23.201.11',]
+#myhosts =  ['172.23.176.123',]
+myhosts =  ['192.168.1.104',]
 
-wwwroot = '/var/www/html/deploy'
-package = os.path.join(wwwroot,'package')
+CODE_REPO_PATH = '/var/www/html/deploy'
+CODE_PACKAGE = os.path.join(CODE_REPO_PATH,'package')
+
+def get_last_ver():
+    with cd(CODE_REPO_PATH):
+        lastver  = run('cat lastvar')
+    return lastver
+
+def get_live_ver():
+    lastver = get_last_ver()
+    with lcd(CODE_REPO_PATH):
+        if not os.path.isfile('livever'):
+            local('echo %s > livever' % lastver)
+            livever = lastver
+        else:
+            livever = run('cat livever')
+    return livever
 
 @hosts(myhosts)
 def deploy():
-    with cd(wwwroot):
-        lastver  = run('cat lastwar')
-        print 'lastver: %s' % lastver
+    lastver = get_last_ver()
+    livever = get_live_ver()
+
+    print lastver, livever
+    if lastver != livever:
+        local('echo %s > livever' % lastver)
     
-    with lcd(wwwroot):
-        if not os.path.isfile('livever'):
-            local('echo %s > livever' % lastver)
-            livever = ""
+    with lcd(CODE_PACKAGE):
+        get(os.path.join(CODE_PACKAGE, 'wordpress-%s.tar' % lastver),'wordpress-%s.tar' % lastver)
+        local_md5 = local('md5sum wordpress-%s.tar' % lastver)
+        get(os.path.join(CODE_PACKAGE, 'wordpress-%s.tar.md5' % lastver),'wordpress-%s.tar.md5' % lastver)
+        remote_md5 = local('cat wordpress-%s.tar.md5' % lastver)
+        if local_md5 == remote_md5 :
+            local('tar -zxvf wordpress-%s.tar' % lastver)
         else:
-            livever = run('cat livever')
+            print 'file distribute error.'
         
-        print lastver, livever
-        if lastver != livever:
-            local('echo %s > livever' % lastver)
-            with lcd(package):
-                get(os.path.join(package,'wordpress-%s.tar' % lastver),'wordpress-%s.tar' % lastver)
-                get(os.path.join(package,'wordpress-%s.tar.md5' % lastver),'wordpress-%s.tar.md5' % lastver)
-        
+        f = os.path.join(CODE_PACKAGE, 'wordpress-%s' % lastver)
+        local('ln -s %s /var/www/html/htdocs ' %  f)
+
